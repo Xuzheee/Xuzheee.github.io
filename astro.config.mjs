85eb878181ca605f402f07c4510aa986375c2485
Xuzheee <huangxuzhe12@163.com>
2026-01-14 12:47:29 +0800

    First deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,12 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
-import tailwind from "@astrojs/tailwind";
+import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://astrofy-template.netlify.app',
-  integrations: [mdx(), sitemap(), tailwind()]
+  // 将此处改为你的 GitHub Pages 网址
+  site: 'https://Xuzheee.github.io',
+  base: '/', 
+  integrations: [mdx(), sitemap(), tailwind()],
 });
