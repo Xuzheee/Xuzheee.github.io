0fde6f9c8c18cc582a5f4cda3ded6cd0a8602d81
Xuzheee <huangxuzhe12@163.com>
2026-04-08 23:43:20 +0800

    update personal site content

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
-  // 将此处改为你的 GitHub Pages 网址
   site: 'https://Xuzheee.github.io',
-  base: '/', 
-  integrations: [mdx(), sitemap(), tailwind()],
+  base: '/',
+  integrations: [mdx(), tailwind()],
 });
